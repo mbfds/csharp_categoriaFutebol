419ec0694c610019578e4821f599b848df1d44e9
mbfds <marcelo.florentino.ti@gmail.com>
2018-11-27 12:10:20 -0200

    Trabalho desenvolvido no modulo de logica de programação

---
+++ ProgramaCategoria/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace ProgramaCategoria
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variável de designer necessária.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpar os recursos que estão sendo usados.
+		/// </summary>
+		/// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código gerado pelo Windows Form Designer
+
+		/// <summary>
+		/// Método necessário para suporte ao Designer - não modifique 
+		/// o conteúdo deste método com o editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.txtNome = new System.Windows.Forms.TextBox();
+			this.txtIdade = new System.Windows.Forms.TextBox();
+			this.txtResultado = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.btnVerificar = new System.Windows.Forms.Button();
+			this.btnLimpar = new System.Windows.Forms.Button();
+			this.label4 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// txtNome
+			// 
+			this.txtNome.Location = new System.Drawing.Point(118, 56);
+			this.txtNome.Name = "txtNome";
+			this.txtNome.Size = new System.Drawing.Size(100, 20);
+			this.txtNome.TabIndex = 0;
+			// 
+			// txtIdade
+			// 
+			this.txtIdade.Location = new System.Drawing.Point(118, 113);
+			this.txtIdade.Name = "txtIdade";
+			this.txtIdade.Size = new System.Drawing.Size(100, 20);
+			this.txtIdade.TabIndex = 1;
+			// 
+			// txtResultado
+			// 
+			this.txtResultado.Location = new System.Drawing.Point(103, 263);
+			this.txtResultado.Name = "txtResultado";
+			this.txtResultado.Size = new System.Drawing.Size(217, 20);
+			this.txtResultado.TabIndex = 2;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(56, 56);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(38, 13);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "Nome:";
+			this.label1.Click += new System.EventHandler(this.label1_Click);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(56, 116);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(37, 13);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "Idade:";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(35, 266);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(58, 13);
+			this.label3.TabIndex = 5;
+			this.label3.Text = "Resultado:";
+			// 
+			// btnVerificar
+			// 
+			this.btnVerificar.Location = new System.Drawing.Point(42, 195);
+			this.btnVerificar.Name = "btnVerificar";
+			this.btnVerificar.Size = new System.Drawing.Size(75, 23);
+			this.btnVerificar.TabIndex = 6;
+			this.btnVerificar.Text = "Verificar";
+			this.btnVerificar.UseVisualStyleBackColor = true;
+			this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+			// 
+			// btnLimpar
+			// 
+			this.btnLimpar.Location = new System.Drawing.Point(140, 195);
+			this.btnLimpar.Name = "btnLimpar";
+			this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+			this.btnLimpar.TabIndex = 7;
+			this.btnLimpar.Text = "Limpar";
+			this.btnLimpar.UseVisualStyleBackColor = true;
+			this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(81, 13);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(100, 13);
+			this.label4.TabIndex = 8;
+			this.label4.Text = "Programa Categoria";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(375, 450);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.btnLimpar);
+			this.Controls.Add(this.btnVerificar);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.txtResultado);
+			this.Controls.Add(this.txtIdade);
+			this.Controls.Add(this.txtNome);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox txtNome;
+		private System.Windows.Forms.TextBox txtIdade;
+		private System.Windows.Forms.TextBox txtResultado;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Button btnVerificar;
+		private System.Windows.Forms.Button btnLimpar;
+		private System.Windows.Forms.Label label4;
+	}
+}
+
---
+++ ProgramaCategoria/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgramaCategoria
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void btnLimpar_Click(object sender, EventArgs e)
+		{
+			txtIdade.Clear();
+			txtNome.Clear();
+			txtResultado.Clear();
+		}
+
+		private void btnVerificar_Click(object sender, EventArgs e)
+		{
+			int idade;
+			
+
+			idade= Convert.ToInt32(txtIdade.Text);
+
+			if (idade < 6)
+			{
+				txtResultado.Text = "Essa pessoa ainda é pequena demais para jogar.";
+
+
+			}else 
+			if(idade<=7){
+				txtResultado.Text = "Chupetinha";
+			}else
+			if (idade <= 9)
+			{
+				txtResultado.Text = "Fraldinha";
+			}else
+			if (idade <= 11)
+			{
+				txtResultado.Text = "Pré-Mirim";
+			}
+			else
+			if (idade <= 13)
+			{
+				txtResultado.Text = "Mirim";
+			}
+			else
+			if (idade <= 15)
+			{
+				txtResultado.Text = "infantil";
+			}
+			else
+			if (idade <= 17)
+			{
+				txtResultado.Text = "Juvenil";
+			}
+			else
+			if (idade <= 39)
+			{
+				txtResultado.Text = "Adulto";
+			}
+			else
+			if (idade <= 49)
+			{
+				txtResultado.Text = "Master";
+			}
+			else
+			{
+				txtResultado.Text = "Sênior";
+			}
+			
+		
+		}
+	}
+}
